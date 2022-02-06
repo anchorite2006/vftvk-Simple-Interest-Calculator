28982bbbc88ee21f708d0945f56547fd398de8ca
anchorite2006 <anchorite2006@gmail.com>
2022-02-06 09:51:27 +0000

    everyone can be rich

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = principal * years * rate / 100;
+    let year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerText = "\n\nIf you deposit " + principal + ",\n\
+    at an interest rate of " + rate + "%.\n\
+    You will receive an amount of " + interest + ",\n\
+    in the year " + year;
+}
+
+function updateRate() {
+    let rate = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rate+'%';
 }
         
